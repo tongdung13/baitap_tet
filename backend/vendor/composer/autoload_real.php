b91629f30d74b9d02e427ab4fe112aaca4af41c1
Dinhtuan1999 <nguyendinhtuan112@gmail.com>
2021-02-04 21:03:42 +0700

    add

--- backend/vendor/composer/autoload_real.php
+++ backend/vendor/composer/autoload_real.php
@@ -22,15 +22,13 @@ class ComposerAutoloaderInitc3b78108511e2c238645a47953ec27e5
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInitc3b78108511e2c238645a47953ec27e5', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitc3b78108511e2c238645a47953ec27e5', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitc3b78108511e2c238645a47953ec27e5::getInitializer($loader));
         } else {
